6e5831a24e816bcbd66eecaf14583d38e7faac87
Gabriel Moreno da Luz <gabrielmluz@hotmail.com>
2025-04-11 22:31:29 -0300

    fix: simplifica configuração do Vercel e remove builds personalizado

--- vercel-build.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process')
-
-// Instala as dependências
-execSync('npm install', { stdio: 'inherit' })
-
-// Executa o build do Quasar
-execSync('quasar build', { stdio: 'inherit' })
